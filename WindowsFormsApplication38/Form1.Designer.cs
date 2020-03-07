1beb334e0ddac67bee48943f27129e9c5d5f39dc
luogy <luogy@LAPTOP-EVLPURG4>
2020-03-07 13:46:00 +0800

    添加项目文件。

---
+++ WindowsFormsApplication38/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication38
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.captureImageBox = new Emgu.CV.UI.ImageBox();
+            this.captureButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captureImageBox
+            // 
+            this.captureImageBox.Location = new System.Drawing.Point(43, 28);
+            this.captureImageBox.Name = "captureImageBox";
+            this.captureImageBox.Size = new System.Drawing.Size(300, 300);
+            this.captureImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.captureImageBox.TabIndex = 2;
+            this.captureImageBox.TabStop = false;
+            // 
+            // captureButton
+            // 
+            this.captureButton.Location = new System.Drawing.Point(422, 113);
+            this.captureButton.Name = "captureButton";
+            this.captureButton.Size = new System.Drawing.Size(148, 76);
+            this.captureButton.TabIndex = 6;
+            this.captureButton.Text = "button1";
+            this.captureButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 324);
+            this.Controls.Add(this.captureButton);
+            this.Controls.Add(this.captureImageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox captureImageBox;
+        private System.Windows.Forms.Button captureButton;
+    }
+}
+
---
+++ WindowsFormsApplication38/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util.TypeEnum;
+
+namespace WindowsFormsApplication38
+{
+    public partial class Form1 : Form
+    {
+        // Capture是Emgu.CV提供的摄像头控制类，里面的ImageGrabbed事件表示的是获取到图片后触发。可以用来实现模拟摄像头视频获取（其实是在picturebox中显示图片，由于很快，就跟视频一样）
+        // Capture另一个非常关键的方法是QueryFrame（）这个方法是用来获取当前的摄像头捕捉到的图面。
+        VideoCapture _capture;
+        Mat frame = new Mat();
+        public Form1()
+        {
+            InitializeComponent();
+            _capture = new VideoCapture(@"C:\Users\Administrator\Desktop\video\vtest.avi");
+           // _capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, 300);
+            //设置捕捉到帧的高度为320。
+           // _capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth,
+            //300);
+            //设置捕捉到帧的宽度为240。
+           // _capture.FlipHorizontal = true;
+            //捕捉到帧数据进行水平翻转。
+            _capture.ImageGrabbed += _capture_ImageGrabbed;
+
+            _capture.Start();
+           
+        }
+
+        void _capture_ImageGrabbed(object sender, EventArgs e)
+        {
+           
+
+            _capture.Retrieve(frame, 0);
+
+            //var frame1 = _capture.Retrieve(frame,0);
+            captureImageBox.Image = frame;
+
+
+
+
+        }
+    }
+}
